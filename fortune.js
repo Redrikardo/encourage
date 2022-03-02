c672faf2a677f524cdd391e8eaad3692b560d31c
Giannis Megoulas <95855227+GiannisMeg@users.noreply.github.com>
2022-03-02 08:41:16 +0100

    Delete fortune.js

--- fortune.js
+++
@@ -1,9 +0,0 @@
-teller = {
-	guess: () => {
-		const millions = Math.random();
-		const message = `you will earn ${millions} euros this year`;
-		console.log(message);
-	},
-};
-console.log(teller.guess());
-// logs undefined as second
